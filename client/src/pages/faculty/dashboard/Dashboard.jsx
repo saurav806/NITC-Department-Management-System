982d6a87d5be50951a2debf96b3ada9825f0dc94
Shivam Kr. Jha <shivamkumarjhadbg6@gmail.com>
2024-07-07 14:42:35 +0530

    fac dahsborad

--- client/src/pages/faculty/dashboard/Dashboard.jsx
+++ client/src/pages/faculty/dashboard/Dashboard.jsx
@@ -3,12 +3,8 @@ import ListProject from '../addProject/ListProject';
 import StudentRequest from '../studentRequest/StudentRequest';
 import './Dashboard.css';
 import Sidebar from '../../../components/sidebar/sidebar';
-<<<<<<< HEAD
 import Profile from '../../../components/profile/profile';
-=======
 import { useAuth } from '../../../store/auth';
->>>>>>> 690ae492321f6a872cb15cc4ceb9f14f10e24d18
-
 const Dashboard = () => {
 
   const {user} = useAuth();
@@ -17,11 +13,7 @@ const Dashboard = () => {
     { name: 'List A Project', component: ListProject },
     { name: 'Student Request', component: StudentRequest },
     { name: 'Your Project(s)', component: () => <div>Your Project(s) Content</div> },
-<<<<<<< HEAD
     { name: 'Profile', component: Profile }
-=======
-    { name: 'Profile', component: () => <div>` This is {user.firstname} {user.lastname}`</div> }
->>>>>>> 690ae492321f6a872cb15cc4ceb9f14f10e24d18
   ];
 
   return (
